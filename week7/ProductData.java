d9c9cc3dc01dd11152ca9340a687c54bb7a68da5
enochou <echou@my.bcit.ca>
2013-10-29 22:44:27 -0700

    Week7 Josh Zhang & Yi-Nung Chou

---
+++ week7/ProductData.java
@@ -0,0 +1,34 @@
+package com.homework;
+
+public class ProductData {
+	public String Id;
+	public String Title;
+	public String Artist;
+	public long Price;
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public String getArtist() {
+		return Artist;
+	}
+	public void setArtist(String artist) {
+		Artist = artist;
+	}
+	public long getPrice() {
+		return Price;
+	}
+	public void setPrice(long price) {
+		Price = price;
+	}
+	
+	
+}
